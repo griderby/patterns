81667fc67db0b653863bcdd16ab53b65705ba477
victar.baranchyk <victar.baranchyk@msiminsk.com>
2018-05-15 20:04:09 +0300

    init

---
+++ chapter-2/ShopProduct.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magep
+ * Date: 12.04.2018
+ * Time: 23:16
+ */
+
+class ShopProduct
+{
+
+}
---
+++ chapter-2/ShopProductWriter.php
@@ -0,0 +1,5 @@
+<?php
+
+class ShopProduct {
+    private $taxFree = 17;
+}
